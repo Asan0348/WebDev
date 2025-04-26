d04924d3c90916b14f606b10033e7a355f634291
Asan0348 <a_abuov@kbtu.kz>
2025-04-26 18:22:47 +0500

    Update views.py

--- lab 10/hh_back/api/views.py
+++ lab 10/hh_back/api/views.py
@@ -5,17 +5,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
-# from .models import *
+
 from .serializers import *
 
 
-# FBV - function based view
-# @api_view([ 'GET'])
-# def all_companies(request, pk=None):
-#     if request.method == "GET":
-#         companies = Company.objects.all()
-#         serializer = CompanySerializer(companies, many=True)
-#         return Response(serializer.data)
+
 
 class AllCompanies(APIView):
     def get(self, request):
@@ -31,10 +25,7 @@ class AllCompanies(APIView):
         return Response(serializer.errors)
 
 
-# def all_companies(request):
-#     companies = Company.objects.all()
-#     companies_json = [c.to_json() for c in companies]
-#     return JsonResponse(companies_json, safe=False)
+
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -60,12 +51,6 @@ def company(request, company_id):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# def company(request, company_id):
-#     try:
-#         c = Company.objects.get(id=company_id)
-#     except Company.DoesNotExist as error:
-#         return JsonResponse({'message: {error}'})
-#     return JsonResponse(c.to_json())
 
 @api_view(['GET', 'POST'])
 def all_vacancies(request, vacancy_id=None):
@@ -82,10 +67,6 @@ def all_vacancies(request, vacancy_id=None):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# def all_vacancies(request):
-#     vacancies = Vacancy.objects.all()
-#     vacancies_json = [v.to_json() for v in vacancies]
-#     return JsonResponse(vacancies_json, safe=False)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def vacancy(request, vacancy_id):
@@ -110,12 +91,6 @@ def vacancy(request, vacancy_id):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# def vacancy(request, vacancy_id):
-#     try:
-#         v = Vacancy.objects.get(id=vacancy_id)
-#     except Vacancy.DoesNotExist as error:
-#         return JsonResponse({'message: {error}'})
-#     return JsonResponse(v.to_json())
 
 
 @api_view([ 'GET'])
@@ -130,9 +105,6 @@ def vacancies_by_company(request, company_id):
         return Response(serializer.data)
 
 
-# def vacancies_by_company(request, company_id):
-#     vacancies_json = [v.to_json() for v in Vacancy.objects.all() if v.company and v.company.id == company_id]
-#     return JsonResponse(vacancies_json, safe=False)
 
 @api_view([ 'GET'])
 def top_ten_vacancies(request):
